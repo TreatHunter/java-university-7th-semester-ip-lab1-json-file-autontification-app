4bac5557ec95c08a99ec1e5b096a76c119e409d6
TreatHunter <michael.zanin.a@gmail.com>
2021-10-09 15:42:34 +0300

    added maven to project

---
+++ src/test/java/org_treathunter/java_university_7th_semester_ip_lab1_json_file_autontification_app/AppTest.java
@@ -0,0 +1,20 @@
+package org_treathunter.java_university_7th_semester_ip_lab1_json_file_autontification_app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
